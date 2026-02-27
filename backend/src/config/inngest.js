77383b7f617df3d6030e2bcd9d3f0fa47a527ee4
Harshit Saini <harshitabcd131@gmail.com>
2026-02-28 01:13:23 +0530

    issue fixed

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,22 +1,21 @@
 
 import { Inngest } from "inngest";
 import { connectDb } from "./db.js";
-
+import User from "../models/User.js";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "call-vid" });
 const syncUser = inngest.createFunction(
     {id:"sync-user"},
     {event:"clerk/user.created"},
 
-    async({Event})=>{
+    async({event})=>{
        await connectDb()
 
-       const {id,email_addresses,first_name,last_name,image_url}=event.data
-    
+       const {id,email_addresses,first_name,last_name,image_url}=event.data    
        const newUser={
         clerkId:id,
         email:email_addresses[0].email_address,
-        name:`$${first_name||""}${last_name||""}`,
+        name:`${first_name||""}${last_name||""}`,
         image:image_url
        }
 
@@ -33,9 +32,7 @@ const deleteUserFromDb=inngest.createFunction(
     async({event})=>{
         await connectDb()
         const {id}=event.data
-        await User.deleteOneO({clerkId:id})
-    }
-
+        await User.deleteOne({clerkId:id})
 )
 
 // Create an empty array where we'll export future Inngest functions
