5c094577919cba15ead676c57acc6b122c1439c2
Harshit Saini <harshitabcd131@gmail.com>
2026-02-28 01:44:17 +0530

    import error fixed

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,7 +1,7 @@
 
 import { Inngest } from "inngest";
 import { connectDb } from "./db.js";
-import User from "../models/User.js";
+import User from "../models/user.model.js";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "call-vid" });
 const syncUser = inngest.createFunction(
